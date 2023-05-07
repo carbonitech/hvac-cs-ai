e58ed5c3e2f966fa12746e6fb5dc200c4b1398b0
carboni-joseph <joe@carbonitech.com>
2023-05-07 17:39:18 -0400

    embedding creation outputs df ready for db

--- app/main.py
+++ app/main.py
@@ -40,6 +40,7 @@ import numpy as np   # for splitting page text into equal size sub-strings
 from dataclasses import dataclass
 import psycopg2 as pg
 from os import getenv
+from typing import Optional
 
 load_dotenv()
 
@@ -50,12 +51,24 @@ openai.api_key = getenv('OPENAI_API_KEY')
 
 @dataclass
 class File:
-    file_data: str|bytes
     entity: str
     category: str
-
+    file_data: Optional[bytes] = None
+    file_path: Optional[str] = None
+    
+    def file_name(self):
+        return self.file_path.split('/')[-1]
 class db:
-    conn = pg.connect(getenv('DATABASE_URL'))
+
+    def __enter__(self):
+        self.conn = pg.connect(getenv('DATABASE_URL'))
+        return self
+    
+    def __exit__(self, e_type, e_val, e_tb):
+        if e_tb:
+            return False
+        self.close()
+        return True
 
     def get_embeddings(self) -> pd.DataFrame:
         sql = "SELECT text, embedding FROM embeddings;"
@@ -70,9 +83,9 @@ class db:
     def post_embeddings(self, values: list[tuple|dict]) -> bool:
         success = False
         if isinstance(values[0], dict):
-            sql = "INSERT INTO embeddings (text, embedding) VALUES (%(text)s, %(embedding)s)"
+            sql = "INSERT INTO embeddings (text, embedding, file_id, category) VALUES (%(text)s, %(embedding)s,%(file_id)s, %(category)s)"
         elif isinstance(values[0], tuple):
-            sql = "INSERT INTO embeddings (text, embedding) VALUES (%s, %s);"
+            sql = "INSERT INTO embeddings (text, embedding, file_id, category) VALUES (%s, %s, %s, %s);"
 
         with self.conn:
             with self.conn.cursor() as curr:
@@ -84,9 +97,35 @@ class db:
                     success = False
                 insert(sql,values)
                 success = True
-        return success 
-    
-    
+        return success
+
+    def add_entity(self, entity_name: str):
+        with self.conn:
+            with self.conn.cursor() as curr:
+                sql = "INSERT INTO entities (name) VALUES (%s) RETURNING id;"
+                curr.execute(sql, (entity_name,))
+                result = curr.fetchone()[0]
+        return result
+
+    def get_entities(self):
+        with self.conn:
+            with self.conn.cursor() as curr:
+                sql = "SELECT * FROM entities;"
+                curr.execute(sql)
+                result = curr.fetchall()
+        if result:
+            entities = pd.DataFrame(result, columns=["id","name"])
+        else:
+            entities = pd.DataFrame()
+        return entities 
+
+    def add_file(self, filename: str, entity: int):
+        with self.conn:
+            with self.conn.cursor() as curr:
+                sql = "INSERT INTO files (name, entity_id) VALUES (%s,%s) RETURNING id;"
+                curr.execute(sql, (filename,entity))
+                return curr.fetchone()[0]
+
     def close(self):
         self.conn.close()
 
@@ -102,7 +141,7 @@ def read_file(file: File):
     Reads in an entire file and splits the text into 3 equal sections per page 
     Returns: a list of text sections
     """
-    reader = PdfReader(file.file_data)
+    reader = PdfReader(file.file_data if file.file_data else file.file_path)
     text_by_page = [page.extract_text() for page in reader.pages]
     text_by_page_split = []
     prefix = np.array([file.entity, file.category])
@@ -117,7 +156,7 @@ def read_file(file: File):
 
     return text_by_page_split
 
-def create_embeddings(text: list, embeddings_model: str) -> pd.DataFrame:
+def create_embeddings_table(embeddings_model: str, entity: int, file: File, database: db) -> pd.DataFrame:
     """Create embeddings for each text segment and return as a DataFrame
         The embeddings model returns a data object of this format
         {
@@ -141,6 +180,7 @@ def create_embeddings(text: list, embeddings_model: str) -> pd.DataFrame:
             }
         }
 """
+    text = read_file(file=file)
     embeddings = []
     for segment in text:
         response = openai.Embedding.create(model=embeddings_model, input=segment)
@@ -148,13 +188,29 @@ def create_embeddings(text: list, embeddings_model: str) -> pd.DataFrame:
             assert i == resp_data_obj["index"]  # double check embeddings are in same order as input
         batch_embeddings = [embedding_object["embedding"] for embedding_object in response["data"]]
         embeddings.extend(batch_embeddings)
-    return pd.DataFrame({'text': text, 'embedding': embeddings})
+
+    result = pd.DataFrame({'text': text, 'embedding': embeddings})
+    result["category"] = file.category
+
+    with database as db_manager:
+        entities = db_manager.get_entities()
+        if not entities.empty:
+            if id_val := entities.loc[entities["name"] == entity,"id"].item():
+                file_id = db_manager.add_file(file.file_name(), id_val)
+        else:
+            entity_id = db_manager.add_entity(entity)
+            file_id = db_manager.add_file(file.file_name(), entity=entity_id)
+        result["file_id"] = file_id 
+            
+    # result has text, embedding, file_id, and category
+    return result
+
 
 def save_results(df: pd.DataFrame, database: db):
     # convert embeddings to strings so they aren't converted to python sets
     df["embedding"] = df["embedding"].apply(str)
     records: list[dict] = df.to_dict(orient="records") 
-    success = database.post_embeddings(records)
+    with database as session:
+        success = session.post_embeddings(records)
     assert success, "Embeddings unable to be saved"
-    database.close()
     return
