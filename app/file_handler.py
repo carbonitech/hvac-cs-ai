dc5e5a592e32d7beddabf8666601177b522b76ac
carboni-joseph <joe@carbonitech.com>
2023-05-11 21:09:55 -0400

    use chunks parameter instead of hardcoded `3`

--- app/file_handler.py
+++ app/file_handler.py
@@ -15,7 +15,7 @@ class File:
 
     def read_and_chunk(self, chunks: int) -> list[str]:
         """
-        Reads in an entire file and splits the text into 3 equal sections per page 
+        Reads in an entire file and splits the text into equal sections (chunks) per page 
         Returns: a list of text sections
         """
         reader = PdfReader(self.file_data if self.file_data else self.file_path)
@@ -27,7 +27,7 @@ class File:
             page = np.array(page.encode('ascii','ignore').decode().split()) 
             split_string = [
                 " ".join(np.concatenate((prefix,section)))
-                for section in np.array_split(page,3)
+                for section in np.array_split(page,chunks)
             ]
             text_by_page_split.extend(list(split_string))
 
