3f587238aec53bd9008f7bf04c2b03dfb62c0f04
carboni-joseph <joe@carbonitech.com>
2023-05-12 20:35:53 -0400

    first test!

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -1,6 +1,10 @@
 from ai.ai import AI
 from random import random, randint
-from app.main import EMBEDDING_MODEL
+from app.main import EMBEDDING_MODEL, GPT_MODEL
+import pandas as pd
+from app.file_handler import File
+from db.db import db
+from os import getenv
 
 class TestAI(AI):
 
@@ -29,3 +33,14 @@ class TestAI(AI):
             }
         }
         return embedding
+
+
+def test_generate_embeddings_table():
+    database = db(connection=getenv('DATABASE_URL'))
+    ai = TestAI(EMBEDDING_MODEL,GPT_MODEL,database)
+    file_path = '/home/carboni/projects/hvac-cs-ai/5e133f6d27f35743210648.pdf'
+    entity = 'ADP'
+    category = 'Warranty'
+    file = File(entity=entity, category=category, file_path=file_path)
+    embeddings_table: pd.DataFrame = ai.generate_embeddings_table(file=file)
+    assert len(embeddings_table) == file.num_pages*3
