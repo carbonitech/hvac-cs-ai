0c11bb26313e71f46ce8b12c164c33763650bccc
carboni-joseph <joe@carbonitech.com>
2023-05-24 08:29:33 -0400

    built out relevance, query building, and chat

--- ai/ai.py
+++ ai/ai.py
@@ -5,16 +5,28 @@ import pandas as pd
 from db.db import db
 from app.file_handler import File
 from scipy import spatial  # for calculating vector similarities for search
+import tiktoken  # for counting tokens
 
 class AI:
 
-    def __init__(self, embedding_model_name: str, gpt_model_name: str, database: db) -> None:
+    def __init__(
+            self,
+            embedding_model_name: str,
+            gpt_model_name: str,
+            database: db,
+            token_limit: int=4096-500
+        ) -> None:
         self.embedding_model_name = embedding_model_name
         self.gpt_model_name = gpt_model_name
         self.db = database
+        self.token_limit = token_limit
 
     def _create_embedding(self, text):
-        """encapsulate the embeddings creation call -- enables mocking for tests"""
+        """
+            Call the OpenAI embeddings engine to get eembedding values for a string
+            Encapsulating this allows for testing with a mock substitute
+            (OpenAI charges per token)
+        """
         return openai.Embedding.create(
             model=self.embedding_model_name,
             input=text
@@ -59,9 +71,63 @@ class AI:
             success = session.post_embeddings(records)
         return success
 
+
     def ranked_strings_by_relatedness(
+            self,
             query: str,
+            doc_embeddings: pd.DataFrame,
             relatedness_fn = lambda x, y: 1 - spatial.distance.cosine(x,y),
-            top_n: int = 100
-    ) -> tuple[list[str], list[float]]:
-        """Returns a list of strings and relatedness, sorted from most related to least"""
+            top_n: int = 5
+    ) -> pd.DataFrame:
+   #) -> tuple[list[str], list[float]]:
+        """Returns a dataframe of the top_n document embeddings in the database, scored and ranked in descending order
+            The dataframe will have columns upon return -> text, embedding, file_id, score"""
+        query_embedding_resp = self._create_embedding(query)
+        query_embedding = query_embedding_resp['data'][0]['embedding']
+        doc_embeddings['score'] = doc_embeddings['embedding'].apply(lambda emb: relatedness_fn(query_embedding, emb))
+        doc_embeddings = doc_embeddings.sort_values(by='score', ascending=False)
+        return doc_embeddings.head(n=top_n)
+
+    def num_tokens(self, text: str|list[str]) -> int:
+        encoding = tiktoken.encoding_for_model(model_name=self.gpt_model_name)
+        if isinstance(text, str):
+            tokens = encoding.encode(text=text)
+            num_tokens = len(tokens)
+        elif isinstance(text, list):
+            tokens = encoding.encode_batch(text=text)
+            num_tokens = 0
+            for batch in tokens:
+                num_tokens += len(batch)
+        return num_tokens
+    
+    def build_query_message(self, query: str) -> str:
+        """Build a message for GPT with an introduction, relevant source texts, and the question/query to answer"""
+        with self.db as session:
+            all_file_embeddings = session.get_embeddings()
+        top_related = self.ranked_strings_by_relatedness(query=query, doc_embeddings=all_file_embeddings)
+        introduction = 'Use the document segments below provided by vendors that explain topics such as warranty policies, product specifications, and installation instructions to answer the subsequent question. If the answer cannot be found in these document segments, write \"Sorry, I could not find an answer.\"\n'
+        question = f"\n\nQuestion: {query}"
+        full_message = introduction
+        for row in top_related.itertuples():
+            next_segment = f'\n\nDocument segment:\n"""\n{row.text}'
+            if self.num_tokens(full_message + next_segment + question) > self.token_limit:
+                break
+            else:
+                full_message += next_segment
+        return full_message
+    
+    def _complete_chat(self, messages: list[dict[str,str]]):
+        return openai.ChatCompletion.create(
+            model=self.gpt_model_name,
+            messages=messages,
+            temperature=0
+        )
+    def ask_model(self, query: str) -> str:
+        """Answers a question (query) using GPT and a database of relevant text segments from uploaded vendor documents"""
+        message = self.build_query_message(query=query)
+        messages = [
+            {"role": "system", "content": "You answer questions about vendor documents in the HVAC industry, which are typically handled by customer service agents"},
+            {"role": "user", "content": message}
+        ]
+        response = self._complete_chat(messages=messages)
+        return response['choices'][0]['message']['content']
