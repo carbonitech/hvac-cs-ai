d19a4a2936a5ac49218601d390354940b4f5dd68
carboni-joseph <joe@carbonitech.com>
2023-05-24 11:02:21 -0400

    moved module docstring

--- ai/ai.py
+++ ai/ai.py
@@ -1,4 +1,31 @@
-"""Interface for the AI"""
+"""Interface for the AI
+
+Borrowing from the OpenAI Cookbook Question/Answering example using embeddings
+
+https://github.com/openai/openai-cookbook/blob/main/examples/Question_answering_using_embeddings.ipynb
+
+Full procedure
+
+1. Prepare search data (once)
+    A. Collect: We'll download a few hundred Wikipedia articles about the 2022 Olympics
+    B.Chunk: Documents are split into short, mostly self-contained sections to be embedded
+    C.Embed: Each section is embedded with the OpenAI API
+    D.Store: Embeddings are saved (for large datasets, use a vector database)
+2. Search (once per query)
+    A.Given a user question, generate an embedding for the query from the OpenAI API
+    B.Using the embeddings, rank the text sections by relevance to the query
+3. Ask (once per query)
+    A.Insert the question and the most relevant sections into a message to GPT
+    B.Return GPT's answer
+    There's no perfect recipe for splitting text into sections.
+
+Regarding the tradeoffs of document splitting strategy gernerally
+    -   Longer sections may be better for questions that require more context
+    -   Longer sections may be worse for retrieval, as they may have more topics muddled together
+    -   Shorter sections are better for reducing costs (which are proportional to the number of tokens)
+    -   Shorter sections allow more sections to be retrieved, which may help with recall
+    -   Overlapping sections may help prevent answers from being cut by section boundaries
+"""
 from dotenv import load_dotenv; load_dotenv()        # expose the OPEN_API_KEY before the openai module attempts to pull it in __init__
 import openai
 import pandas as pd
