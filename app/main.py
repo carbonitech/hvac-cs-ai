b5ac1c5fcf2e07e93706b080ab92e7b59d1ad4f2
carboni-joseph <joe@carbonitech.com>
2023-06-06 16:44:21 -0400

    allow CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.resources.files import files
 from app.resources.chat import chat
 
 app = FastAPI()
 app.include_router(files)
 app.include_router(chat)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/test')
+def test_route(param1: str):
+    return {'data': param1}
